50ad6ea01db575fb002ce39fa280cbbfdfae19b3
Philippe quesnel <philippe.quesnel@gmail.com>
2017-05-02 23:14:27 -0400

    fixed paths in txt2autokey.sh

--- examples/mtgap-beakPQ/bias-10/txt2autokey.sh
+++ examples/mtgap-beakPQ/bias-10/txt2autokey.sh
@@ -2,4 +2,4 @@
 
 # ps: use links here for include files (?)
 
-../../txt2autokey ../../qwertyMain30.txt kbd.txt includes.ahk | tee mtgap-beakPQ.ahk
+../../../txt2autokey ../../../qwertyMain30.txt kbd.txt includes.ahk | tee mtgap-beakPQ.ahk
